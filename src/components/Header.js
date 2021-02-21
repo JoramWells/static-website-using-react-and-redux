7f2b7df6b30628d5cc836f54125fe3f9a75d1aed
bramuel <jorammanoah1@gmail.com>
2021-02-21 11:28:08 -0800

    Header Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import './index.css'
+import logo from './logo.png'
 
 export default function Header (){
     return(
 
         <div className="nv">
+                        <h1 >Justice and Peace Center</h1>
+
+            <div className="col-sm-12">
+            <hr />
+            </div>
+
             <nav className="navbar1">
-                <div className="brand">JPC-Kitale</div>
+                <div className="brand">
+                    <img src={logo} alt="Justice and Peace Center logo" style={{width:"70px", height:"70px", borderRadius:"90px"}} />
+                </div>
                 <a href="#" className="toggle">
                     <span className="bar"></span>
                     <span className="bar"></span>
